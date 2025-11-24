ffe86094cacbae7955e924c19a2c9fad8f76caa4
antoine mouille <antoine_mouille@ens.univ-artois.fr>
2025-11-24 11:27:05 +0100

    fix app

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
         <Navbar />
         <main className="flex-grow ">
           <Routes>
-            <Route path="/portoflio" element={<Home />} />
+            <Route path="/portfolio" element={<Home />} />
             <Route path="/projects" element={<Projects />} />
             <Route path="/CV" element={<CV/>}/>
             <Route path="/contact" element={<Contact />} />
